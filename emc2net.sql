325bb122449e440dacaaf8bcc89261580755b03c
Senan <jhernan33@gmail.com>
2018-02-17 12:51:47 -0400

    Update select
    Select cpu, memory, harddisk, graphics card, Pc

--- emc2net.sql
+++ emc2net.sql
@@ -105,3 +105,96 @@ INNER JOIN emc2net.cpu AS c ON (a.cpu_id = c.cpu_id)
 INNER JOIN emc2net.harddisk AS d ON (a.disco_id = d.disco_id)
 INNER JOIN emc2net.grahicscard AS e ON (a.tgrafica = e.tgraf_id)
 order by precio desc;
+
+-- Consulta 2
+SELECT
+a.pcid,
+b.men_tipo,
+b.mem_capacidad AS mem_MB,
+c.cpu_fabricante AS cpu_fab,
+c.cpu_tipo,
+d.disco_fabricante AS disco_fab,
+d.disco_capacidad AS disco_GB,
+e.tgraf_fabricante AS tgraf_fab,
+a.precio
+FROM emc2net.pc AS a
+left outer JOIN emc2net.memory AS b ON (a.mem_id = b.mem_id)
+left outer JOIN emc2net.cpu AS c ON (a.cpu_id = c.cpu_id)
+left outer JOIN emc2net.harddisk AS d ON (a.disco_id = d.disco_id)
+left outer JOIN emc2net.grahicscard AS e ON (a.tgrafica = e.tgraf_id)
+order by precio desc;
+
+--Obtener una relación de solo los modelos de 'PC NO completos' a la venta.
+-- Queremos toda la información disponible sobre los componentes que lo forman
+-- Consulta 3
+(SELECT
+a.pcid,
+b.men_tipo,
+b.mem_capacidad AS mem_MB,
+c.cpu_fabricante AS cpu_fab,
+c.cpu_tipo,
+d.disco_fabricante AS disco_fab,
+d.disco_capacidad AS disco_GB,
+e.tgraf_fabricante AS tgraf_fab,
+a.precio
+FROM emc2net.pc AS a
+left outer JOIN emc2net.memory AS b ON (a.mem_id = b.mem_id)
+left outer JOIN emc2net.cpu AS c ON (a.cpu_id = c.cpu_id)
+left outer JOIN emc2net.harddisk AS d ON (a.disco_id = d.disco_id)
+left outer JOIN emc2net.grahicscard AS e ON (a.tgrafica = e.tgraf_id)
+)
+except
+(
+SELECT
+a.pcid,
+b.men_tipo,
+b.mem_capacidad AS mem_MB,
+c.cpu_fabricante AS cpu_fab,
+c.cpu_tipo,
+d.disco_fabricante AS disco_fab,
+d.disco_capacidad AS disco_GB,
+e.tgraf_fabricante AS tgraf_fab,
+a.precio
+FROM emc2net.pc AS a
+inner JOIN emc2net.memory AS b ON (a.mem_id = b.mem_id)
+inner JOIN emc2net.cpu AS c ON (a.cpu_id = c.cpu_id)
+inner JOIN emc2net.harddisk AS d ON (a.disco_id = d.disco_id)
+inner JOIN emc2net.grahicscard AS e ON (a.tgrafica = e.tgraf_id)
+)
+order by precio desc;
+
+-- Consulta 4
+SELECT
+a.pcid,
+b.men_tipo,
+b.mem_capacidad AS mem_MB,
+c.cpu_fabricante AS cpu_fab,
+c.cpu_tipo,
+d.disco_fabricante AS disco_fab,
+d.disco_capacidad AS disco_GB,
+e.tgraf_fabricante AS tgraf_fab,
+a.precio
+FROM emc2net.pc AS a
+left outer JOIN emc2net.memory AS b ON (a.mem_id = b.mem_id)
+left outer JOIN emc2net.cpu AS c ON (a.cpu_id = c.cpu_id)
+left outer JOIN emc2net.harddisk AS d ON (a.disco_id = d.disco_id)
+left outer JOIN emc2net.grahicscard AS e ON (a.tgrafica = e.tgraf_id)
+where c.cpu_fabricante like '%amd%'
+and d.disco_fabricante like '%samsung%'
+order by precio desc;
+
+-- Consulta 5
+--Obtener una relación del numero de PCs que tienen CPUs de Intel y de AMD. Ordenar de mayor a menor.
+
+select * 
+from emc2net.pc as a
+inner join emc2net.cpu as b on(a.cpu_id = b.cpu_id);
+
+-- Agrupar por fabricante
+select 
+b.cpu_fabricante,
+count(*) as total
+from emc2net.pc as a
+inner join emc2net.cpu as b on (a.cpu_id = b.cpu_id)
+group by b.cpu_fabricante
+order by total desc;
