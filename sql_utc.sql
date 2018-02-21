5c43dc4a1be94137b5bc2462f0edd32072918deb
Senan <jhernan33@gmail.com>
2018-02-21 13:11:26 -0400

    select utc
    Uct, timestamp

--- sql_utc.sql
+++ sql_utc.sql
@@ -2,3 +2,9 @@
 select current_timestamp;
 -- Verificar la Zona horaria de la Zona
 select utc_offset from pg_timezone_names where name='America/Caracas';
+
+SELECT TIMESTAMP WITH TIME ZONE '2009-11-06 17:05:01' AT TIME ZONE 'UTC-6'; --&gt; Rpta: "2009-11-07 04:05:01"
+SELECT TIMESTAMP WITH TIME ZONE '2009-11-06 17:05:01' AT TIME ZONE 'UTC-7'; --&gt; Rpta: "2009-11-07 05:05:01"
+SELECT TIMESTAMP WITH TIME ZONE '2009-11-06 17:05:01' AT TIME ZONE 'UTC-8'; --&gt; Rpta: "2009-11-07 06:05:01"
+SELECT TIMESTAMP WITH TIME ZONE '2009-11-06 17:05:01' AT TIME ZONE 'MST'; --&gt; Rpta: "2009-11-06 15:05:01"
+SELECT TIMESTAMP WITH TIME ZONE '2009-11-06 17:05:01' AT TIME ZONE 'PST'; --&gt; Rpta: "2009-11-06 14:05:01"
