d48e3e8f23d2dee1fea104a0e17bed7b9607e485
Senan <jhernan33@gmail.com>
2018-02-22 11:34:54 -0400

    Sql Time Venezuela
    Select time utc-4 of Venezuela

---
+++ sql_time_utc4.sql
@@ -0,0 +1,13 @@
+﻿begin;
+select now();
+select now() at time zone 'GMT';
+select now() at time zone 'GMT+1';
+select now() at time zone 'PST';
+select now() at time zone 'UTC+4'; -- VENEZUELA
+end;
+-- Extract day from now
+select extract(day from now());
+select extract(dow from now());
+select to_char(current_date, 'Day');
+select to_char(now(), 'Day');
+select to_char(current_date-4, 'Day');
