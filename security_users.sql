7c089d540847c1697504a94720c8659bb2ecc2d0
Senan <jhernan33@gmail.com>
2018-02-28 15:56:34 -0400

    Security save password
    Password Encripted

---
+++ security_users.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO security.users(
+            pers_cedu, user_codi, user_name, user_emai, user_pass, user_eras, 
+            acco_codi)
+    values(
+            13506468, 001, 'friends', 'friends47@gmail.com', digest('12345','sha512'), 
+            false, 
+            1);
+
+select oid, * from public.persons;
+
+select * from security.users;
+select oid from security.users;
